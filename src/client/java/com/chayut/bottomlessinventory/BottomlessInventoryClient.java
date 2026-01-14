ecf62710f70902f7b0fef8fdbe48c269fb3eab5f
Chayut <chun.chayut@gmail.com>
2026-01-14 21:56:45 +0700

    Init

---
+++ src/client/java/com/chayut/bottomlessinventory/BottomlessInventoryClient.java
@@ -0,0 +1,10 @@
+package com.chayut.bottomlessinventory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BottomlessInventoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
